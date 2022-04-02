9556689e7137a4e76c2e865e1a389b37f39b7a76
Fpress225 <sidorovkirll320@gmail.com>
2022-04-02 17:06:44 +0300

    New2.0

--- Dly_Sanechki.c
+++ Dly_Sanechki.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
 int main(void) {
     printf("Hello, Sanechka. Eto vse dly teby. Vozmi kalkylator\n");
-    printf("Zadymai lyboe cheloe chislo. U ymnoch ego na 6\n");
+    printf("Zadymai lyboe cheloe chislo, krome 0. U ymnoch ego na 6\n");
     printf("Podeli ego na 2\n");
     printf("Vedi polychenor chislo:\n");
-    int x, y, t;
+    int y, t, x;
     scanf("%d", &x);
     y = x * 2;
     t = y / 6;
-    printf("Teper magic. I ygaday chislo.Gotov?\n");
-    printf("Tvoe chislo: %d\n", t );
+    if(t != 0 ){
+        printf("Teper magic. I ygaday chislo.Gotov?\n");
+        printf("Tvoe chislo: %d\n", t );
+    }
+    else if ( x == 0){
+        printf("Ti eblan? I skazal LYBOEEE CHISLO KROME 0!!!!!!!!!\n");
+
+    }
+
+
     return 0;
 }
