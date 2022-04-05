26c6e7abe11ffa2893b613b1cc058053359ac75d
Fpress225 <sidorovkirll320@gmail.com>
2022-04-05 11:34:10 +0300

    Задание с циклом. Доработано)))

--- sanbox/GG.c
+++ sanbox/GG.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main(void) {
-    int f, g; 
+    int f, g, ee, rr; 
     for (int r = 666; r < 701; r++ || f; 699 < f < 721; f++ || g; 719 <g<741;g++) {
         if ( r < 701 ) {
             printf("%d\n", r);
             if ( r = 700 ) {
                 f = 700;
                 r = 701;
+                printf("Нажми энтер");
+                scanf ("%d", ee);
             }
         } 
         else if( 699 < f < 721 ) { 
@@ -15,6 +17,8 @@ int main(void) {
             if ( f = 720 ){
                 g = 720;
                 f = 721;
+                printf("Нажми энтер");
+                scanf ("%d", rr);
             }
         }  
         else if ( 719 < g < 741 ) {
