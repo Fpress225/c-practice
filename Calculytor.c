6e7f9268bb02e0f36bc4720c37ec2c683e0848cb
Fpress225 <sidorovkirll320@gmail.com>
2022-04-13 18:30:52 +0300

    Сделал все что вспомнил

--- Calculytor.c
+++ Calculytor.c
@@ -3,42 +3,41 @@
 void flush_input(void);
 
 int main() {
-    printf("\n");
-    float one, two;
-    char deistvie;
-    printf("Введи первое число(пример 0.0): ");
-    scanf("%f", &one);
-    flush_input();
+    for(;;) {
+        printf("\n");
+        float one, two;
+        char deistvie;
+        printf("Введи первое число(пример 0.0): ");
+        scanf("%f", &one);
+        flush_input();
 
-    printf("Введи второе число(пример 0.0): ");
-    scanf("%f", &two);
-    flush_input();
+        printf("Введи второе число(пример 0.0): ");
+        scanf("%f", &two);
+        flush_input();
 
-    printf("Введи действие(+, -, /, *): ");
-    scanf("%c", &deistvie);
+        printf("Введи действие(+, -, /, *): ");
+        scanf("%c", &deistvie);
 
-    float vicheslenie;
-    switch(deistvie) {
-        case '+':
-            vicheslenie = one + two;
-            printf("%.3f + %.3f = %.3f\n", one, two, vicheslenie);
-            break;
-        case '-':
-            vicheslenie = one - two;
-            printf("%.3f - %.3f = %.3f\n", one, two, vicheslenie);
-            break;
-        case '*':
-            vicheslenie = one * two;
-            printf("%.3f * %.3f = %.3f\n", one, two, vicheslenie);
-            break;
-        case '/':
-            vicheslenie = one / two;
-            printf("%.3f / %.3f = %.3f\n", one, two, vicheslenie);
-            break;
-        defualt: 
-            printf("Ты что-то сделал не то)");
+        float vicheslenie;
+        switch(deistvie) {
+            case '+':
+                vicheslenie = one + two;
+                printf("%.3f + %.3f = %.3f\n", one, two, vicheslenie);
+                break;
+            case '-':
+                vicheslenie = one - two;
+                printf("%.3f - %.3f = %.3f\n", one, two, vicheslenie);
+                break;
+            case '*':
+                vicheslenie = one * two;
+                printf("%.3f * %.3f = %.3f\n", one, two, vicheslenie);
+                break;
+            case '/':
+                vicheslenie = one / two;
+                printf("%.3f / %.3f = %.3f\n", one, two, vicheslenie);
+                break;
+        }
     }
-
     return 0;
 }
 
